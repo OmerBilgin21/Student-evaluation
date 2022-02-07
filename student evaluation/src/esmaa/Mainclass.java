65e42c50775e3b07a3fcb161280d909aa39bad4e
Ömer Faruk Bilgin <75097600+moldere@users.noreply.github.com>
2022-02-08 02:09:06 +0300

    Student evaluator and lister
    Student evaluation takes the students name's and grade's and gives you the most successful and the least students and also prepares a frequency graph which allows you to see the grade status clearly

---
+++ student evaluation/src/esmaa/Mainclass.java
@@ -0,0 +1,13 @@
+package esmaa;
+
+public class Mainclass {
+
+	public static void main(String[] args) {
+		
+		Registersinifi register = new Registersinifi();
+		
+		register.kayitlar();
+
+	}
+
+}
